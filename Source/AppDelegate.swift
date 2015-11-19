f8bf8c92e2c7daf679b31f49cd307fe60193b280
itscory <corey@stewartcompanies.co>
2015-11-18 20:43:56 -0500

    added function headers that need to be coded sometime

--- Source/AppDelegate.swift
+++ Source/AppDelegate.swift
@@ -65,3 +65,49 @@ extension String {
 }
 
 
+// Below are some unfinished functions that needed
+
+func mountPartition(path:String) throws {
+	
+	if false {
+		// If partition is already mounted
+		throw MountError.AlreadyMounted
+	}
+	
+	if false {
+		// If partition has invalid filesystem
+		throw MountError.NoMountableFilesystems
+	}
+}
+
+func unmountPartition(path:String) throws {
+	
+	if false {
+		// if partition is being used
+		throw MountError.ResourceBusy
+	}
+}
+
+func driveIsSSD(path:String) -> Bool {
+	/*
+		SSD drives must be handled differently using the Secure Erase feature. Therefore, it is nessesary to detect whether a drive is SSD or something else. The function should return true if the given drive is Solid-State.
+	*/
+	return false
+}
+
+func driveIsStartup(path:String) -> Bool {
+	/*
+		Returns true if the given drive is the startup volume.
+	*/
+	return false
+}
+
+func partitionIsSystem(path:String) -> Bool {
+	/*
+		This is needed in order to protect the system being used to run this app.
+		Returns true if the given partition is the system volume.
+	*/
+	return false
+}
+
+
